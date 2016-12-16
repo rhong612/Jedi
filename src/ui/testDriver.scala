47d3428907ee07da08a647e77052269233fbbeb8
rhong612 <rhong612@gmail.com>
2016-12-15 16:09:47 -0800

    Moved tester code to testDriver

--- src/ui/testDriver.scala
+++ src/ui/testDriver.scala
@@ -5,14 +5,545 @@ import values._
 
 object testDriver {
   def main(args : Array[String]) : Unit = {
-    Number.test
-    Boole.test
     check
     check2
-    //EwokParsers.test
-    //WookieParsers.test
-    SithParsers.test
+    parserTest
  }
+  
+ def parserTest() {
+	 println("------------TESTING NUMBER PARSER----------------")
+    val sithParser = new SithParsers
+    var expression = "+10"
+    var tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: 10")
+    println("Actual: " + tree.get)
+    expression = "-10"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: " + expression)
+    println("Actual: " + tree.get)
+    expression = "5.2142"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: " + expression)
+    println("Actual: " + tree.get)
+    expression = "4912"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: " + expression)
+    println("Actual: " + tree.get)
+    expression = "0"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: " + expression)
+    println("Actual: " + tree.get)
+    expression = "+9999"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: 9999")
+    println("Actual: " + tree.get)
+    expression = "123"
+    tree = sithParser.parseAll(sithParser.number, expression)
+    println("Expected: " + expression)
+    println("Actual: " + tree.get)
+    
+    
+    println("------------TESTING BOOLE PARSER----------------")
+    expression = "true"
+    var bTree = sithParser.parseAll(sithParser.boole, expression)
+    println("Expected: " + expression)
+    println("Actual: " + bTree.get)
+    expression = "false"
+    bTree = sithParser.parseAll(sithParser.boole, expression)
+    println("Expected: " + expression)
+    println("Actual: " + bTree.get)
+    
+    
+    
+    
+    println("------------TESTING IDENTIFIER PARSER----------------")
+    expression = "aVarName"
+    var iTree = sithParser.parseAll(sithParser.identifier, expression)
+    println("Expected: " + expression)
+    println("Actual: " + iTree.get)
+    expression = "withNumbers01231"
+    iTree = sithParser.parseAll(sithParser.identifier, expression)
+    println("Expected: " + expression)
+    println("Actual: " + iTree.get)
+    expression = "numbersIn1Middle2"
+    iTree = sithParser.parseAll(sithParser.identifier, expression)
+    println("Expected: " + expression)
+    println("Actual: " + iTree.get)
+    expression = "a"
+    iTree = sithParser.parseAll(sithParser.identifier, expression)
+    println("Expected: " + expression)
+    println("Actual: " + iTree.get)
+    
+    
+    println("------------TESTING LITERAL PARSER----------------")
+    expression = "true"
+    var lTree = sithParser.parseAll(sithParser.literal, expression)
+    println("Expected: " + expression)
+    println("Actual: " + lTree.get)
+    expression = "4.5124"
+    lTree = sithParser.parseAll(sithParser.literal, expression)
+    println("Expected: " + expression)
+    println("Actual: " + lTree.get)
+    
+    
+    println("------------TESTING OVERALL PARSER----------------")
+    val globalEnv = new Environment()
+    expression = "3 + 4 * 5"
+    var eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: " + 23)
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "def b2 = 3 + 4 * 5"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "5"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: " + 5)
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "true || false"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: true")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "b2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 23")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "def b1 = 4"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "b1 < b2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: true")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "b1 == b2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: false")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "def b3 = if (b1 == b2) 3 else 4"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "b3"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 4")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "def b4 = if (b1 < b2) 7 else 10"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    expression = "b4"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 7")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    
+    expression = "def a1 = 2 + 3 * 5"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 17.0")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "def a2 = (2 + 3) * 5"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 25.0")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1 == a2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: false")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1 < a2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: true")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1 < a2 && true && a1 == a2 && a1 == a3"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: false")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1 == a2 || false || a1 < a2 || a1 == a3"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: true")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a1 == a3"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Undefined Identifier: a3")
+    try {
+      println("Actual: " + eTree.get.execute(globalEnv)) 
+    }
+    catch {
+      case e : UndefinedException => println("Actual: " + e.msg)
+    }
+    expression = "def a3 = if (a1 == a2) a4 else 0"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a3"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 0.0")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "a4"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Undefined Identifier: a4")
+    try {
+      println("Actual: " + eTree.get.execute(globalEnv)) 
+    }
+    catch {
+      case e : UndefinedException => println("Actual: " + e.msg)
+    }
+    expression = "3 - 2 - 1"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 0.0")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    expression = "100 / 10 / 2"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 5.0")
+    println("Actual: " + eTree.get.execute(globalEnv))
+    
+    
+    println("------------TESTING LAMBDA AND BLOCK----------------")
+    val newEnv = new Environment
+    expression = "def square = lambda (x) x * x"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    expression = "square(3)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 9.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    expression = "def f2c = lambda (ft) {def c = 5 / 9; c * (ft - 32)}"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "f2c(212)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 100.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    expression = "c"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Undefined identifier: c")
+    try {
+      println("Actual: " + eTree.get.execute(newEnv)) 
+    }
+    catch {
+      case e : UndefinedException => println("Actual: " + e.msg)
+    }
+    
+    expression = "def addN = lambda (n) lambda (x) x + n"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    expression = "def add6 = addN(6)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    expression = "add6(3)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 9.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "def abs = lambda(x) if (x < 0) -1 * x else x"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "abs(-9)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 9.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "def delta = 100"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "def small = {def delta = 0.00001; lambda(x) abs(x) < delta}"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "small(-0.00000001)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: true")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "small(0.001)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: false")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "def fact = lambda (n) if (n == 0) 1 else n * fact(n - 1)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "fact(5)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 120.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "def fourCaller = lambda (x) x(4)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: ok")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "fourCaller(lambda (y) y + 1)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 5.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    expression = "(lambda (z) 2 * z) (3)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 6.0")
+    println("Actual: " + eTree.get.execute(newEnv))
+    
+    
+    println("------------TESTING VARIABLES, ITERATION, AND ASSIGNMENT----------------")
+    val env = new Environment
+    expression = "def count = var(0)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "count"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Variable(0.0)")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[count]"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 0.0")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "count = [count] + 1"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: DONE")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[count]"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 1.0")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "while([count] < 100) count = [count] + 1"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: DONE")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[count]"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 100.0")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "def fun = var(lambda(x) x * x)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[fun](3)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 9.0")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "fun = lambda(x) 2 * x"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: DONE")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[fun](3)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 6.0")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "def more = var(true)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "more = false"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: done")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "more = 0"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: done")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "def countRef = var(count)"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: OK")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "countRef"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Variable(Variable(100.0))")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[countRef]"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: Variable(100.0)")
+    println("Actual: " + eTree.get.execute(env))
+    
+    expression = "[[countRef]]"
+    eTree = sithParser.parseAll(sithParser.expression, expression)
+    println()
+    println(expression)
+    println("Expected: 100.0")
+    println("Actual: " + eTree.get.execute(env))
+ }
+  
   
  def check2() {
     
